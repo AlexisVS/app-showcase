7e626f4ac510b5aa47dec050519760f7a44efa77
AlexisVS <alexis.vansan1440@gmail.com>
2024-08-01 15:04:05 +0200

    added logic for fully auto component import

--- src/app/in/app.component.ts
+++ src/app/in/app.component.ts
@@ -1,67 +1,29 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {Showcase, Component as ComponentShowcase} from '../_types/showcaseType';
-import {
-	EqDateRangeComponent,
-	EqDateTimeComponent,
-	EqDateComponent,
-	EqM2oComponent,
-	EqStringComponent,
-	EqTextComponent,
-	// @ts-ignore
-} from 'sb-shared-lib';
 
-import eqText from '../component-doc/eq-text.json';
-import eqString from '../component-doc/eq-string.json';
-import eqM2o from '../component-doc/eq-m2o.json';
-import eqDateTime from '../component-doc/eq-date-time.json';
-import eqDateRange from '../component-doc/eq-date-range.json';
-import eqDate from '../component-doc/eq-date.json';
+import {showcases} from '../../showcases';
 
 @Component({
 	selector: 'app-app',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-	public showcases: Record<string, { showcase: Showcase }> = {
-		eqString: {showcase: eqString},
-		eqText: {showcase: eqText},
-		eqDate: {showcase: eqDate},
-		eqDateTime: {showcase: eqDateTime},
-		eqDateRange: {showcase: eqDateRange},
-		eqM2o: {showcase: eqM2o},
-	};
+	public showcases: Showcase[] = showcases;
 
 	public forceComponentToRefresh = false;
 
 	/* Property for displaying the background grid */
 	public showBackgroundGrid = false;
 
-
-	public ngOnInit(): void {
-		this.showcases.eqString.showcase.componentSelector = EqStringComponent;
-		this.showcases.eqText.showcase.componentSelector = EqTextComponent;
-		this.showcases.eqDate.showcase.componentSelector = EqDateComponent;
-		this.showcases.eqDateTime.showcase.componentSelector = EqDateTimeComponent;
-		this.showcases.eqDateRange.showcase.componentSelector = EqDateRangeComponent;
-		this.showcases.eqM2o.showcase.componentSelector = EqM2oComponent;
-	}
-
-	get showcasesKeys(): string[] {
-		return Object.keys(this.showcases);
-	}
-
 	public toggleComponentsMode(): void {
-		for (const key in this.showcases) {
-			if (!this.showcases.hasOwnProperty(key)) {
-				continue;
-			}
 
-			this.showcases[key].showcase.components.forEach((component: ComponentShowcase): void => {
+		this.showcases.forEach((showcase: Showcase): void => {
+			showcase.components.forEach((component: ComponentShowcase): void => {
 				component.properties.mode = component.properties.mode === 'view' ? 'edit' : 'view';
 			});
-		}
+		});
 
 		this.forceComponentToRefresh = !this.forceComponentToRefresh;
 
